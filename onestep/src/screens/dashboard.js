4f8621a4798375ea7e64a048767ac4b2097f56bb
Bruce Quach <quachbruce@gmail.com>
2023-04-22 12:32:47 -0500

    List all tasks as array objects

--- onestep/src/screens/dashboard.js
+++ onestep/src/screens/dashboard.js
@@ -2,11 +2,150 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { ToggleButtonGroup, ToggleButton, } from '@mui/material';
 
+function Task({ task, onCompletion }) {
+    return (
+        <div>
+            <h3>{task.name}</h3>
+            <p>{task.description}</p>
+            <button onClick={() => onCompletion(task.id)}>Completed</button>
+        </div>
+    )
+}
+
 const Dashboard = () => {
 
     let navigate = useNavigate();
 
     const [currentDifficulty, setCurrentDifficulty] = useState('easy');
+
+    const handleEasyTaskCompletion = (taskId) => {
+        const updatedTasks = easyTaskArray.map((task) =>
+          task.id === taskId ? { ...task, completed: true } : task
+        );
+        setEasyTaskArray(updatedTasks);
+    };
+    
+    const handleMediumTaskCompletion = (taskId) => {
+        const updatedTasks = mediumTaskArray.map((task) =>
+          task.id === taskId ? { ...task, completed: true } : task
+        );
+        setMediumTaskArray(updatedTasks);
+    };
+    
+    const handleHardTaskCompletion = (taskId) => {
+        const updatedTasks = hardTaskArray.map((task) =>
+          task.id === taskId ? { ...task, completed: true } : task
+        );
+        setHardTaskArray(updatedTasks);
+    };
+
+    const [easyTaskArray, setEasyTaskArray] = useState([
+        {
+            id: 1,
+            name: 'Anti-Litterbug',
+            description: 'Pick up 10 pieces of litter',
+            difficulty: 'easy',
+            completed: false
+        },
+        {
+            id: 2,
+            name: 'The Responsible',
+            description: 'Turns the lights off before leaving the house',
+            difficulty: 'easy',
+            completed: false
+        },
+    ]);
+
+
+    const [mediumTaskArray, setMediumTaskArray] = useState([
+        {
+            id: 1,
+            name: 'Vegetarian Explorer',
+            description: 'Switch to a vegetarian or vegan diet for the day.',
+            difficulty: 'medium',
+            completed: false
+        },
+        {
+            id: 2,
+            name: 'Bulbous',
+            description: 'Switch light bulbs in house to energy efficient bulbs.',
+            difficulty: 'medium',
+            completed: false
+        },
+        {
+            id: 3,
+            name: 'E-Mail',
+            description: 'Switch to paperless billing and communication for bills and documents.',
+            difficulty: 'medium',
+            completed: false
+        },
+        {
+            id: 4,
+            name: 'The Electrician (Lite)',
+            description: 'Go without using any electricity for 3 hours outside of sleeping hours',
+            difficulty: 'medium',
+            completed: false
+        },
+    ]);
+
+    const [hardTaskArray, setHardTaskArray] = useState([
+        {
+            id: 1,
+            name: 'Joyride',
+            description: 'Choose to walk, bike, or take public transportation instead of driving a car for the day.',
+            difficulty: 'hard',
+            completed: false
+        },
+        {
+            id: 2,
+            name: 'Composter',
+            description: 'Compost fruits, vegetables, or other biodegradables instead of throwing them out.',
+            difficulty: 'hard',
+            completed: false
+        },
+        {
+            id: 3,
+            name: 'Green Thumb',
+            description: 'Compost fruits, vegetables, or other biodegradables instead of throwing them out.',
+            difficulty: 'hard',
+            completed: false
+        },
+        {
+            id: 4,
+            name: 'Market Farmer',
+            description: "Shop at a local farmer's market or co-op for locally grown produce.",
+            difficulty: 'hard',
+            completed: false
+        },
+        {
+            id: 5,
+            name: 'Reusable',
+            description: 'Reduce plastic usage by bringing a reusable water bottle and shopping bag.',
+            difficulty: 'hard',
+            completed: false
+        },
+        {
+            id: 6,
+            name: 'Old School Laundromat',
+            description: 'Use a clothesline or drying rack instead of a dryer for one load of laundry.',
+            difficulty: 'hard',
+            completed: false
+        },
+        {
+            id: 7,
+            name: 'Communal Cleaning',
+            description: 'Attend a community clean-up event to reduce litter and pollution.',
+            difficulty: 'hard',
+            completed: false
+        },
+        {
+            id: 8,
+            name: 'The Electrician',
+            description: 'Go without using any electricity for 6 hours outside of sleeping hours',
+            difficulty: 'hard',
+            completed: false
+        },
+    ]);
 
     return (
         <div style={styles.dashboardPage}>
@@ -37,14 +176,35 @@ const Dashboard = () => {
                     </div>
                 </div>
                 <div>
-                    { currentDifficulty === 'easy' ?
-                        <h2>Easy</h2>
-                    : currentDifficulty === 'medium' ?
-                        <h2>Medium</h2>
-                    : currentDifficulty === 'hard' ?
-                        <h2>Hard</h2>
-                    :
-                        <h2>Pick something</h2>
+                    {currentDifficulty === 'easy' ?
+                        <div>
+                            <h2>Easy</h2>
+                            <div style={styles.taskList}>
+                                {easyTaskArray.map((task) => (
+                                    <Task key={task.id} task={task} onCompletion={handleEasyTaskCompletion} />
+                                ))}
+                            </div>
+                        </div>
+                        : currentDifficulty === 'medium' ?
+                            <div>
+                                <h2>Medium</h2>
+                                <div style={styles.taskList}>
+                                    {mediumTaskArray.map((task) => (
+                                        <Task key={task.id} task={task} onCompletion={handleMediumTaskCompletion} />
+                                    ))}
+                                </div>
+                            </div>
+                            : currentDifficulty === 'hard' ?
+                                <div>
+                                    <h2>Hard</h2>
+                                    <div style={styles.taskList}>
+                                        {hardTaskArray.map((task) => (
+                                            <Task key={task.id} task={task} onCompletion={handleHardTaskCompletion} />
+                                        ))}
+                                    </div>
+                                </div>
+                                :
+                                <h2>Pick something</h2>
                     }
                 </div>
             </div>
@@ -80,5 +240,9 @@ let styles = {
         margin: 'auto',
         alignItems: 'center',
         justifyContent: 'space-between'
+    },
+    taskList: {
+        display: 'flex',
+        flexDirection: 'column'
     }
 }
