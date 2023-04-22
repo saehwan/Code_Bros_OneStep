0102d78cd93928603c6c309e5140e8f1b96d7bb7
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2023-04-22 16:02:30 -0500

    Update login.js

--- onestep/src/screens/login.js
+++ onestep/src/screens/login.js
@@ -1,15 +1,63 @@
-import { useNavigate }  from 'react-router-dom';
+//import React, { useState } from 'react';
+import React, {useState} from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+//import LoginPage from './components/LoginPage';
 
-const Login = () => {
+//database Connection
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { auth, database } from "./firebase";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
-    let navigate = useNavigate();
 
-    return (
-        <div>
-            <h2>Hi</h2>
-            <button onClick={() => navigate('/dashboard')}>Dashboard</button>
-        </div>
-    );
+function LoginPage(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //props.onSignIn(email, password);
+    //handleSignIn(email, password);
+    props.onSignIn(email,password);
+  }
+
+  function handleSignIn(email, password) {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Sign-in successful
+        const user = userCredential.user;
+        console.log(`Signed in as ${user.email}`);
+        // call the onSignIn callback function to update the app state
+        setIsSignedIn(true);
+      })
+      .catch((error) => {
+        // Sign-in failed
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(`Sign-in failed: ${errorCode} ${errorMessage}`);
+      });
+  }
+  
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login;
+export default LoginPage;
